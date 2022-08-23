414482081e28b47993ee3ad4f077b91a851a9947
Ivan Korostylov <ivan.korostylyov@stryker.com>
2022-08-23 15:33:11 +0300

    remove unused method from user_provider.

--- providers/data/users_provider.py
+++ providers/data/users_provider.py
@@ -4,23 +4,14 @@ from models.users import User
 
 class UsersProvider:
     
-    @staticmethod
     def fake_user():
         return User(
             login = 'somelogin123',
             password = 'fake_password'
         )
     
-    @staticmethod
     def existing_user():
         return User(
             login = 'defunkt',
             password = 'password'
         )
-    
-    @staticmethod
-    def existing_user_from_env():
-        return {
-            'login': os.environ.get("EXISTING_GITHUB_USER_LOGIN"),
-            'id': os.environ.get("EXISTING_GITHUB_USER_ID")
-        }
